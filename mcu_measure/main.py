6525fbb475a35f00a25c0b9fc92fe75b215ed077
vadgoblin@arch <vadgoblin@github.com>
2026-03-08 23:55:39 +0100

    update main for measurment at the window

--- mcu_measure/main.py
+++ mcu_measure/main.py
@@ -1,30 +1,48 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, SPI
 from veml7700 import Veml7700
 from time import sleep_ms, sleep
 from save_measurements import save_measurements
+
+import sdcard, os, vfs
+
+# Constants
+SPI_BUS = 1
+SCK_PIN = 7
+MOSI_PIN = 6
+MISO_PIN = 5
+CS_PIN = 1
+SD_MOUNT_PATH = '/sd'
+
+try:
+    # Init SPI communication
+    spi = SPI(SPI_BUS, sck=Pin(SCK_PIN), mosi=Pin(MOSI_PIN), miso=Pin(MISO_PIN))
+    cs = Pin(CS_PIN)
+    sd = sdcard.SDCard(spi, cs)
+    # Mount microSD card
+    vfs.mount(sd, SD_MOUNT_PATH)
+    # List files on the microSD card
+    print(os.listdir(SD_MOUNT_PATH))
+
+except Exception as e:
+    print('An error occurred:', e)
 
 
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400_000)
 veml = Veml7700(i2c)
 
-measurements = {}
-for it in (25, 50, 100, 200, 400, 800):
-    veml.set_it(it)
-    for gain in (0.125, 0.25, 1, 2):
-        veml.set_gain(gain)
-        sleep_ms(it* 2)
 
-        raws = []
-        for _ in range(5):
-            sleep_ms(it)
+def measure_and_save():
+    measurements = {}
+    for it in (25, 50, 100, 200, 400, 800):
+        veml.set_it(it)
+        for gain in (0.125, 0.25, 1, 2):
+            veml.set_gain(gain)
+            sleep_ms(it * 2)
+
             raw = veml.read_value()["raw"]
-            raws.append(raw)
-        avg = sum(raws) / len(raws)
-        measurements[(it, gain)] = avg
+            measurements[(it, gain)] = raw
 
-save_measurements(measurements)
+    save_measurements(measurements,directory="/sd/measurements")
 
-# turn on built in led to signal that the measurement is finished
-pin = Pin(8, mode=Pin.OUT)
-pin.value(0)
-sleep(60*60)
+while True:
+    measure_and_save()
