f9032dd3c8ac820f197155ebd0e61c73207cae3c
vadgoblin@arch <vadgoblin@github.com>
2026-03-08 18:17:18 +0100

    implement basic measurement creation

--- mcu_measure/main.py
+++ mcu_measure/main.py
@@ -1,31 +1,20 @@
-from machine import Pin
-from multi_veml7700 import MultiVeml7700
+from machine import Pin, I2C
+from veml7700 import Veml7700
 from time import sleep_ms, sleep
-from average_sensor_readings import average_sensor_readings
-from save_measurement import save_measurement
+
 
 if __name__ == '__main__':
-    multi_veml = MultiVeml7700(0, scl=Pin(9), sda=Pin(8), freq=400_000, count=3)
+    i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400_000)
+    veml = Veml7700(i2c)
 
     measurements = {}
     for it in (25, 50, 100, 200, 400, 800):
-        multi_veml.set_it(it)
+        veml.set_it(it)
         for gain in (0.125, 0.25, 1, 2):
-            multi_veml.set_gain(gain)
+            veml.set_gain(gain)
             sleep_ms(it * 2)
 
-            measurement_to_avg = []
-            for i in range(10):
-                measurement = multi_veml.read_values()
-                measurement_to_avg.append(measurement)
-
-            measurement_avg = average_sensor_readings(measurement_to_avg)
-
-
-            measurements[(it,gain)] = measurement_avg
-
-    save_measurement(measurements)
-    p = Pin(8,mode=Pin.OUT)
-    p.value(0)
-
-    sleep(99999)
+            measurement = veml.read_value()
+            raw = measurement['raw']
+            normalized_raw = raw / (it * gain)
+            print(f"IT: {it:3d} ms | Gain: {gain:5.3f} | raw: {raw:8.0f} | raw_norm: {normalized_raw:8.2f}")
